0ba4d1657d9c5f1e96c3d95b4c7e0722d507946e
Joaquin Aguirre <joaquinaguirre17@gmail.com>
2026-08-06 18:03:33 -0300

    AA

--- src/Componentes/Home/Home.jsx
+++ src/Componentes/Home/Home.jsx
@@ -1,28 +1,29 @@
+import Cart from "../cart/Cart";
 import Navbar from "../Navbar/Navbar";
 import "./Home.css";
 
 const Home = () => {
   return (
     <>
-      <Navbar />
+
 
       <section className="hero">
         <div className="hero-content">
           <h1>Bienvenido a Que Gusto</h1>
           <p>
-            Comidas caseras, pizzas, empanadas,
-            hamburguesas y mucho más.
+            Empanadas, minutas,
+            pizzas, sandwiches y hamburguesas.
           </p>
           <button>Ver Menú</button>
         </div>
       </section>
-
+      <Cart />
       <section className="categories">
         <div id="empanadas" className="category-card">
           🥟 Empanadas
         </div>
-        <div id="guarniciones" className="category-card">
-          🍟 Guarniciones
+        <div id="minutas" className="category-card">
+          🍟 Minutas
         </div>
         <div id="pizzas" className="category-card">
           🍕 Pizzas
--- src/Componentes/Productos/Productos.jsx
+++
@@ -1,9 +0,0 @@
-const Products = () => {
-  return (
-    <div>
-      <h2>Productos</h2>
-    </div>
-  );
-};
-
-export default Products;
